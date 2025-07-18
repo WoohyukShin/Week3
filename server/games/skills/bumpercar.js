bf75e62e4880456773393786cd9c16d5b4b805e8
Sunmin-Lim <imsnmn24@gmail.com>
2025-07-18 13:52:58 +0900

    add skills

---
+++ server/games/skills/bumpercar.js
@@ -0,0 +1,24 @@
+const Skill = require('../skills');
+
+class bumpercar extends skills {
+  constructor(owner) {
+    super(owner);
+    this.name = '노래';
+    this.description = '범퍼카 재생! 다른 사람 commit 게이지 절반으로. 1회 사용.';
+    this.cooldown = 9999; // 사실상 1회용
+    this.used = false;
+  }
+
+  execute(allPlayers) {
+    if (this.used) return;
+    allPlayers.forEach(player => {
+      if (player !== this.owner && player.isAlive) {
+        player.commitGauge /= 2;
+      }
+    });
+    this.used = true;
+    this.onUse();
+  }
+}
+
+module.exports = bumpercar;
